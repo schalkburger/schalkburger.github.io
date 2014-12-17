a78a39787ea49aa2e7c65a426fcd8f246a402a5c
Schalk Burger <hello@schalkburger.za.net>
2014-12-17 15:04:32 +0200

    Schalk Github Page 1.3 Fixes

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projects Template | Schalk Burger Web Developer</title>
+	<meta name="description" content="Schalk Burger Projects Template">
+    <meta name="author" content="Schalk Burger">
+    <link rel="icon" href="img/favicon.ico">    
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <header class="header">
+		<div class="navbar navbar-inverse" role="navigation">
+      		<div class="container">
+        		<div class="navbar-header">
+          			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			            <span class="sr-only">Toggle navigation</span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+			            <span class="icon-bar"></span>
+         			</button>
+         			<a class="navbar-brand" href="http://www.schalkburger.za.net/" title="Schalk Burger">Schalk Burger</a>						
+    			</div>
+		        <div class="collapse navbar-collapse">
+		          <ul class="nav navbar-nav">            
+		            <li><a href="http://www.schalkburger.za.net/about-schalk-burger">About</a></li>
+		            <li><a href="http://www.schalkburger.za.net/contact">Contact</a></li>
+		            <li><a href="http://www.schalkburger.za.net/projects">Projects</a></li>				
+		          </ul>
+		        </div><!--/.nav-collapse -->					
+     		</div>
+   		</div>
+	</header>
+		
+		<div class="jumbotron">
+		<div class="container">
+	 		<h1>Project title</h1>
+			<p>Project description.</p>
+		</div>
+		</div>			
+
+    <div class="container">
+
+      <div class="layout">
+        <h1>Projects</h1>
+        <p class="lead">Hello world! This is the default projects template.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante nisi, sit amet fermentum erat. Suspendisse vel nunc a massa consequat iaculis. Sed pellentesque vehicula elit. Mauris sodales mollis molestie. Integer ultrices laoreet ante, et faucibus sem cursus non. Suspendisse laoreet suscipit vestibulum. Integer mi augue, hendrerit ut fermentum ut, accumsan eget justo. Nam fringilla dapibus risus sed consequat. Nullam lacus urna, pellentesque et laoreet sit amet, volutpat sit amet dolor. Praesent a ultrices velit. Nulla facilisi. Ut sagittis dapibus bibendum. Proin id enim eros. Curabitur tincidunt lorem sed purus semper vitae tincidunt purus aliquet. Integer nulla metus, vestibulum at tincidunt at, euismod sed ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+
+				<p>Integer eget justo neque, eu tristique erat. Vivamus vel bibendum mi. Maecenas ac nunc sollicitudin est viverra ultrices. Quisque sed semper ipsum. Aliquam eu sagittis nibh. Praesent eu elit nisi. Pellentesque augue velit, hendrerit scelerisque pretium non, tristique non arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer facilisis semper nisl, nec rutrum ante tempor id. Donec sapien nisl, varius a aliquet blandit, dapibus quis tortor.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante nisi, sit amet fermentum erat. Suspendisse vel nunc a massa consequat iaculis. Sed pellentesque vehicula elit. Mauris sodales mollis molestie. Integer ultrices laoreet ante, et faucibus sem cursus non. Suspendisse laoreet suscipit vestibulum. Integer mi augue, hendrerit ut fermentum ut, accumsan eget justo. Nam fringilla dapibus risus sed consequat. Nullam lacus urna, pellentesque et laoreet sit amet, volutpat sit amet dolor. Praesent a ultrices velit. Nulla facilisi. Ut sagittis dapibus bibendum. Proin id enim eros. Curabitur tincidunt lorem sed purus semper vitae tincidunt purus aliquet. Integer nulla metus, vestibulum at tincidunt at, euismod sed ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+
+				<p>Integer eget justo neque, eu tristique erat. Vivamus vel bibendum mi. Maecenas ac nunc sollicitudin est viverra ultrices. Quisque sed semper ipsum. Aliquam eu sagittis nibh. Praesent eu elit nisi. Pellentesque augue velit, hendrerit scelerisque pretium non, tristique non arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer facilisis semper nisl, nec rutrum ante tempor id. Donec sapien nisl, varius a aliquet blandit, dapibus quis tortor.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante nisi, sit amet fermentum erat. Suspendisse vel nunc a massa consequat iaculis. Sed pellentesque vehicula elit. Mauris sodales mollis molestie. Integer ultrices laoreet ante, et faucibus sem cursus non. Suspendisse laoreet suscipit vestibulum. Integer mi augue, hendrerit ut fermentum ut, accumsan eget justo. Nam fringilla dapibus risus sed consequat. Nullam lacus urna, pellentesque et laoreet sit amet, volutpat sit amet dolor. Praesent a ultrices velit. Nulla facilisi. Ut sagittis dapibus bibendum. Proin id enim eros. Curabitur tincidunt lorem sed purus semper vitae tincidunt purus aliquet. Integer nulla metus, vestibulum at tincidunt at, euismod sed ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+
+				<p>Integer eget justo neque, eu tristique erat. Vivamus vel bibendum mi. Maecenas ac nunc sollicitudin est viverra ultrices. Quisque sed semper ipsum. Aliquam eu sagittis nibh. Praesent eu elit nisi. Pellentesque augue velit, hendrerit scelerisque pretium non, tristique non arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer facilisis semper nisl, nec rutrum ante tempor id. Donec sapien nisl, varius a aliquet blandit, dapibus quis tortor.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante nisi, sit amet fermentum erat. Suspendisse vel nunc a massa consequat iaculis. Sed pellentesque vehicula elit. Mauris sodales mollis molestie. Integer ultrices laoreet ante, et faucibus sem cursus non. Suspendisse laoreet suscipit vestibulum. Integer mi augue, hendrerit ut fermentum ut, accumsan eget justo. Nam fringilla dapibus risus sed consequat. Nullam lacus urna, pellentesque et laoreet sit amet, volutpat sit amet dolor. Praesent a ultrices velit. Nulla facilisi. Ut sagittis dapibus bibendum. Proin id enim eros. Curabitur tincidunt lorem sed purus semper vitae tincidunt purus aliquet. Integer nulla metus, vestibulum at tincidunt at, euismod sed ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+
+				<p>Integer eget justo neque, eu tristique erat. Vivamus vel bibendum mi. Maecenas ac nunc sollicitudin est viverra ultrices. Quisque sed semper ipsum. Aliquam eu sagittis nibh. Praesent eu elit nisi. Pellentesque augue velit, hendrerit scelerisque pretium non, tristique non arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer facilisis semper nisl, nec rutrum ante tempor id. Donec sapien nisl, varius a aliquet blandit, dapibus quis tortor.</p>
+				
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet ante nisi, sit amet fermentum erat. Suspendisse vel nunc a massa consequat iaculis. Sed pellentesque vehicula elit. Mauris sodales mollis molestie. Integer ultrices laoreet ante, et faucibus sem cursus non. Suspendisse laoreet suscipit vestibulum. Integer mi augue, hendrerit ut fermentum ut, accumsan eget justo. Nam fringilla dapibus risus sed consequat. Nullam lacus urna, pellentesque et laoreet sit amet, volutpat sit amet dolor. Praesent a ultrices velit. Nulla facilisi. Ut sagittis dapibus bibendum. Proin id enim eros. Curabitur tincidunt lorem sed purus semper vitae tincidunt purus aliquet. Integer nulla metus, vestibulum at tincidunt at, euismod sed ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+
+				<p>Integer eget justo neque, eu tristique erat. Vivamus vel bibendum mi. Maecenas ac nunc sollicitudin est viverra ultrices. Quisque sed semper ipsum. Aliquam eu sagittis nibh. Praesent eu elit nisi. Pellentesque augue velit, hendrerit scelerisque pretium non, tristique non arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer facilisis semper nisl, nec rutrum ante tempor id. Donec sapien nisl, varius a aliquet blandit, dapibus quis tortor.</p>
+      </div>
+	  
+    </div>
+		<!-- /.container -->
+		
+		<footer class="footer">
+			<div class="container">
+				<p>&copy; <?php echo date('Y')?> Schalk Burger</p>
+			</div>
+		</footer>	 
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script> 
+  </body>
+</html>
